0b80db051d0483ea9c02d25f01ae82dfbdf40f6b
Macabe <macabe@Macabes-iMac.local>
2022-03-03 15:53:20 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets1.lottiefiles.com/packages/lf20_xyt21aja.json'
+  })
