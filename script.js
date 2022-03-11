98bb4751029d2f714a0df5011e09dabc6815e8cb
Macabe <mfkelly2016@gmail.com>
2022-03-11 18:35:10 -0500

    img src changed for logo

--- script.js
+++
@@ -1,7 +0,0 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('anim'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'https://assets1.lottiefiles.com/packages/lf20_xyt21aja.json'
-  })
